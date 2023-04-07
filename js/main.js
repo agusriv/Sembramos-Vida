1b6909ee596477abb418bbeb3a41eb2f07dae922
Agus Rivera <agustinriveratapi3@gmail.com>
2023-04-07 14:01:39 -0300

    email JS

--- js/main.js
+++ js/main.js
@@ -160,3 +160,45 @@ sr.reveal(`.home__social`, {delay: 600})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
 sr.reveal(`.about2__content, .services__card, .product__card, .questions__group, .footer`,{interval: 100})
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+
+  // Check if the field has a value
+  if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+    // Add and remove color
+    contactMessage.classList.remove('color-blue')
+    contactMessage.classList.add('color-red')
+
+    // Show message
+    contactMessage.textContent = 'Write all the input fields 📩'
+  }else{
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_aooqxbj','template_2s4krds','#contact-form','CKyk1qsCbiaUEVs9j')
+      .then(() =>{
+        // Show message and add color
+        contactMessage.classList.add('color-blue')
+        contactMessage.textContent = 'Message sent ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+          contactMessage.textContent = ''
+        }, 5000)
+      }, (error) =>{
+        alert('OOPS! SOMETHING HAS FAILED...', error)
+      })
+
+      // To clear the input field
+      contactName.value = ''
+      contactEmail.value = ''
+      contactProject.value = ''
+  }
+}
+contactForm.addEventListener('submit', sendEmail)
